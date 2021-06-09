529841cc8adcd9a8e755c05e8af0dd855fe476df
Caimao Feng <caimao.feng@nokia-sbell.com>
2021-06-09 17:10:44 +0800

    add interval

--- shell/install_farm_and_harvester.sh
+++ shell/install_farm_and_harvester.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
-FARMER_NODE=""
-HARVESTER_NODE=[]
+function check_cmd() {
+  cmd=$1
+  if command -v $cmd >/dev/null 2>&1; then
+    return 0
+  else
+    echo "$cmd not installed."
+    return 1
+  fi
+}
 
+function setup_docker_related() {
+  check_cmd docker
+  if [[ $? == "1" ]]; then
+    echo "Installing docker ..."
+    curl -sSL https://get.docker.com/ | sh
+  else
+    echo "Docker already installed, continue..."
+  fi
+  check_cmd docker-compose
+    if [[ $? == "1" ]]; then
+      echo "Installing docker-compose"
+      curl -L \
+      "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" \
+      -o /usr/local/bin/docker-compose
+    else
+      echo "docker-compose already installed, continue..."
+    fi
+}
+setup_docker_related
+
+
+cd docker
+docker build --network=host -t chia-docker .
+cd -
